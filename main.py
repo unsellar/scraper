b277bd3ab92c91eba991d995f0f0c6b43e47a9c8
unsellar <blinov.ceva2015@yandex.ru>
2024-07-10 17:38:06 +0300

    added needed parts recognition

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from scraper import Scraper
+import re
 
 def main():
-    test_url = "http://olympus.realpython.org/profiles/aphrodite"
-    test_
+    test_url = "http://olympus.realpython.org/profiles/poseidon"
+    test_keyword = "title"
 
+    #get html page
     scr = Scraper()
+    html = scr.scrap_html(test_url)
 
-    html = scr.scrap_html()
+    #find keyword
+    keyword = test_keyword
+    pattern = "<" + test_keyword + ".*?>.*?</" + test_keyword + ".*?>"
+    match_results = re.search(pattern, html, re.IGNORECASE)
+    needed_part = match_results.group()
+    needed_part = re.sub("<.*?>", "", needed_part) # Remove HTML tags
 
-    print(html)
+    print(needed_part)
 
-if __name__ == 
+if __name__ == "__main__":
+    main()
